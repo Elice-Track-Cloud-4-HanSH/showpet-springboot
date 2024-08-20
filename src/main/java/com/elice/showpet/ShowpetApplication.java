7e634a2d68481626b4b624f4745b7c2a0d0a410e
PromotezCitizen <gkstkdgus821@gmail.com>
2024-08-20 16:23:13 +0900

    first commit

---
+++ src/main/java/com/elice/showpet/ShowpetApplication.java
@@ -0,0 +1,13 @@
+package com.elice.showpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowpetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/showpet/ShowpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.showpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

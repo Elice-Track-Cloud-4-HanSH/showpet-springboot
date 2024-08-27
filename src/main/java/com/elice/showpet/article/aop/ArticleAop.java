6ecd7a40352b4ff637dd991477b678fc74d876b8
[CLOUD4] 한상현 <gkstkdgus821@gmail.com>
2024-08-27 12:04:25 +0900

    Fix: Article의 요청을 기록하는 AOP 작성

---
+++ src/main/java/com/elice/showpet/article/aop/ArticleAop.java
@@ -0,0 +1,36 @@
+package com.elice.showpet.article.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class ArticleAop extends AopUtils {
+  @Pointcut("execution( * com.elice.showpet.article.*.*.*(..))")
+  public void pointcut() {}
+
+  @Before("pointcut()")
+  public void logBeforeExecute(JoinPoint joinPoint) {
+    String args = argsToString(joinPoint.getArgs());
+    log.info(
+      "method: {}::{}{}",
+      joinPoint.getTarget().getClass().getSimpleName(),
+      joinPoint.getSignature().getName(),
+      args.isEmpty() ? "" : ", args: " + args
+    );
+  }
+
+  @AfterThrowing(pointcut = "pointcut()", throwing = "ex")
+  public void logAfterThrowing(JoinPoint joinPoint, Exception ex) {
+    log.error("method: {}::{}, error: {}",
+      joinPoint.getTarget().getClass().getSimpleName(),
+      joinPoint.getSignature().getName(),
+      ex.getMessage());
+  }
+}

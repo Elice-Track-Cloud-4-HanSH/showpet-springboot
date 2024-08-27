2e9040fb00815ad7395a25ad5aebf899472d4fd5
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-08-27 20:58:35 +0900

    feat: Valid 처리 로직 추가

--- src/main/java/com/elice/showpet/comment/controller/CommentViewController.java
+++ src/main/java/com/elice/showpet/comment/controller/CommentViewController.java
@@ -1,28 +1,51 @@
 package com.elice.showpet.comment.controller;
 
+import com.elice.showpet.article.entity.Article;
+import com.elice.showpet.article.service.ArticleViewService;
 import com.elice.showpet.comment.dto.CommentRequestDto;
+import com.elice.showpet.comment.dto.CommentResponseDto;
 import com.elice.showpet.comment.exception.CommentNotFoundException;
 import com.elice.showpet.comment.service.CommentViewService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class CommentViewController {
 
     private final CommentViewService commentViewService;
-
-    @Autowired
-    public CommentViewController(CommentViewService commentViewService) {
-        this.commentViewService = commentViewService;
-    }
+    private final ArticleViewService articleViewService;
 
     // 댓글 생성
     // http://localhost:8080/comments?articleId=1
     @PostMapping("/comments")
-    public String getAllComments(@RequestParam("articleId") Long articleId, @ModelAttribute CommentRequestDto commentRequestDto) {
+    public String getAllComments(
+            @RequestParam("articleId") Long articleId,
+            @Validated @ModelAttribute CommentRequestDto commentRequestDto,
+            BindingResult bindingResult,
+            Model model) {
+
+        if(bindingResult.hasErrors()){
+            String errorMessage = bindingResult.getFieldError().getDefaultMessage();
+            model.addAttribute("errorMessage", errorMessage);
+
+            Article article = articleViewService.getArticle(articleId);
+            model.addAttribute("article", article);
+
+            List<CommentResponseDto> comments = commentViewService.getAllComments(articleId);
+            model.addAttribute("comments", comments);
+
+            return "article/article";
+        }
+
         commentViewService.createComment(articleId, commentRequestDto);
         return "redirect:/articles/" + articleId;
 
@@ -34,8 +57,7 @@ public class CommentViewController {
     public String updateComment(
             @PathVariable("commentId") Long commentId,
             @RequestParam("articleId") Long articleId,
-            @ModelAttribute CommentRequestDto commentRequestDto) {
-
+            @Validated @ModelAttribute CommentRequestDto commentRequestDto) {
         commentViewService.updateComment(commentId, commentRequestDto);
         return "redirect:/articles/" + articleId;
 
@@ -54,7 +76,7 @@ public class CommentViewController {
         }
     }
 
-    @ExceptionHandler(CommentNotFoundException.class)
+    @ExceptionHandler({CommentNotFoundException.class, IllegalArgumentException.class})
     private String handleCommentNotFoundException() {
         return "error";
     }

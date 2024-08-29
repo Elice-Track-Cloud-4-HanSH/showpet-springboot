b28c23ad26510ef221d24b5331f1717673f5829e
[CLOUD4] 허수빈 <anmedi3623@naver.com>
2024-08-29 12:51:22 +0900

    fix: 서비스에서 비즈니스 로직 수행, 반환값 변경

--- src/main/java/com/elice/showpet/comment/service/CommentViewService.java
+++ src/main/java/com/elice/showpet/comment/service/CommentViewService.java
@@ -1,51 +1,71 @@
 package com.elice.showpet.comment.service;
 
+import com.elice.showpet.article.entity.Article;
+import com.elice.showpet.article.repository.ArticleJdbcTemplateRepository;
 import com.elice.showpet.comment.dto.CommentRequestDto;
+import com.elice.showpet.comment.dto.CommentResponseDto;
 import com.elice.showpet.comment.entity.Comment;
 import com.elice.showpet.comment.exception.CommentNotFoundException;
 import com.elice.showpet.comment.mapper.CommentMapper;
 import com.elice.showpet.comment.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommentViewService {
 
     private final CommentMapper commentMapper;
     private final CommentRepository commentRepository;
-
-    @Autowired
-    public CommentViewService(CommentRepository commentRepository, CommentMapper commentMapper) {
-        this.commentRepository = commentRepository;
-        this.commentMapper = commentMapper;
-    }
+    private final ArticleJdbcTemplateRepository articleRepository;
 
     // 전체 댓글 조회
-    public List<Comment> getAllComments(Long articleId) {
-        return commentRepository.getAllComments(articleId);
+    public List<CommentResponseDto> getAllComments(Long articleId) {
+        articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("not found article: " + articleId));
+        List<Comment> comments = commentRepository.getAllComments(articleId);
+        return comments.stream()
+                .map(commentMapper::commentToCommentResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    // 댓글 조회
+    public CommentResponseDto getComment(Long commentId) {
+        Comment comment = commentRepository.getComment(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("not found comment: " + commentId));
+        return commentMapper.commentToCommentResponseDto(comment);
     }
 
     // 댓글 생성
     @Transactional
-    public void createComment(Long articleId, CommentRequestDto commentRequestDto) {
+    public Long createComment(Long articleId, CommentRequestDto commentRequestDto) {
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new IllegalArgumentException("not found article: " + articleId));
+
         Comment comment = commentMapper.commentRequestDtoToComment(commentRequestDto);
-        commentRepository.upsertComment(articleId, comment);
+        comment.setArticle(article);
+
+        Comment createdComment = commentRepository.upsertComment(comment);
+        return createdComment.getId();
     }
 
     // 댓글 수정
     @Transactional
-    public void updateComment(Long commentId, CommentRequestDto commentRequestDto) {
+    public Long updateComment(Long commentId, CommentRequestDto commentRequestDto) {
         Comment comment = commentRepository.getComment(commentId).
                 orElseThrow(() -> new CommentNotFoundException("not found comment: " + commentId));
 
         Optional.ofNullable(commentRequestDto.getContent())
                 .ifPresent(comment::setContent);
 
-        commentRepository.upsertComment(comment);
+        Comment updatedComment = commentRepository.upsertComment(comment);
+        return updatedComment.getId();
     }
 
     // 댓글 삭제
@@ -58,7 +78,7 @@ public class CommentViewService {
 
     // 게시글 삭제 시 댓글 전체 삭제
     @Transactional
-    public void deleteAllComments(Long articleId){
+    public void deleteAllComments(Long articleId) {
         commentRepository.deleteAllComments(articleId);
     }
 }

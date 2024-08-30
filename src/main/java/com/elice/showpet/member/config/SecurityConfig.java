d7e67755a62fa80e845e88b89c4ebdf32615916c
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-08-30 17:23:06 +0900

    faet : securityconfig 수정

--- src/main/java/com/elice/showpet/member/config/SecurityConfig.java
+++ src/main/java/com/elice/showpet/member/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.elice.showpet.member.config;
 import com.elice.showpet.member.filter.CustomAuthenticationFilter;
 import com.elice.showpet.member.filter.CustomAuthenticationManager;
 import com.elice.showpet.member.filter.CustomAuthenticationProvider;
+import com.elice.showpet.member.filter.CustomSecurityFilter;
 import com.elice.showpet.member.persistence.MemberRepository;
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,6 +19,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +28,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -36,71 +42,60 @@ import java.lang.reflect.Member;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer
+                        .frameOptions(FrameOptionsConfig::disable) // h2 console 을 위함
                 )
-                .addFilterAfter(customAuthenticationFilter(http), CsrfFilter.class)
-                .authenticationManager(authenticationManager(http))
-                .authenticationProvider(authenticationProvider()) // Custom AuthenticationProvider 설정
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/member/register", "/member/login/error", "/h2-console/**").permitAll()
-                                .anyRequest().authenticated()
+                                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                                .requestMatchers(
+                                        "/static/**",
+                                        "/actuator/**",
+                                        "/img/**",
+                                        "/css/**",
+                                        "/member/register",
+                                        "/member/login/error",
+                                        "/member/profile",
+                                        "/h2-console/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
                 )
                 .formLogin(formLogin ->
                         formLogin
                                 .loginPage("/member/login")
+                                .defaultSuccessUrl("/member/profile")
                                 .permitAll()
                 )
                 .logout(logout ->
                         logout
                                 .logoutSuccessUrl("/member/login?logout")
                                 .permitAll()
-                );
+                )
+        ;
+
+        http.addFilterBefore(
+                new CustomSecurityFilter(userDetailsService()), UsernamePasswordAuthenticationFilter.class
+        );
+
         return http.build();
     }
 
     @Bean
-    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http) throws Exception {
-        CustomAuthenticationFilter filter = new CustomAuthenticationFilter(
-                new AntPathRequestMatcher("/member/login", HttpMethod.POST.name())
-        );
-        filter.setAuthenticationManager(authenticationManager(http));
-        filter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler("/member/profile"));
-        filter.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler("/member/login/error"));
-        return filter;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService())
-                .passwordEncoder(passwordEncoder())
-                .and()
-                .build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        return new CustomAuthenticationProvider(userDetailsService(), passwordEncoder());
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService(memberRepository);
     }
-
 
 }
